ac08826902761b6fc72255157322ded6fcb7ec34
Suh Kim <suhlikesgreenlake@gmail.com>
2019-04-16 14:09:34 -0700

    fix a expense summary style (p -> h1)

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,8 +7,7 @@ import visibleExpenses from '../selectors/expenses';
 export const ExpensesSummary = (props) => {
     return(
         <div>
-            {<p>Viewing {props.expenses.length} totalling {numeral(selectExpensesTotal(props.expenses)/100).format('$0,0.00')}
-            </p>}
+            {<h1>Viewing {props.expenses.length} totalling {numeral(selectExpensesTotal(props.expenses)/100).format('$0,0.00')}</h1>}
         </div>)
 }
 
