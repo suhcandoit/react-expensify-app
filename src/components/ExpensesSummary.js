92f36e4af330e26d19f87e7d54b1021956273eaa
Suh Kim <suhlikesgreenlake@gmail.com>
2019-04-16 14:14:24 -0700

    fix expense summary text (expense vs expenses for single vs multiple expenses)

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,9 +5,10 @@ import selectExpensesTotal from '../selectors/expenses-total';
 import visibleExpenses from '../selectors/expenses';
 
 export const ExpensesSummary = (props) => {
+    const expenseWord = props.expenses.length === 1 ? 'expense' : 'expenses';
     return(
         <div>
-            {<h1>Viewing {props.expenses.length} totalling {numeral(selectExpensesTotal(props.expenses)/100).format('$0,0.00')}</h1>}
+            {<h1>Viewing {props.expenses.length} {expenseWord} totalling {numeral(selectExpensesTotal(props.expenses)/100).format('$0,0.00')}</h1>}
         </div>)
 }
 
