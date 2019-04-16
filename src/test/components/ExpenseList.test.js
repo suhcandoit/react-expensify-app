b269e38e7371051387bd54d068b553c8aeb0faab
Suh Kim <suhlikesgreenlake@gmail.com>
2019-04-15 22:48:25 -0700

    formatting amount and createdAt

--- src/test/components/ExpenseList.test.js
+++ src/test/components/ExpenseList.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import { ExpenseList } from '../../components/ExpenseList';
 import expenses from '../fixtures/expenses';
 
+
 test('Should render ExpenseList with expenses', () => {
     const wrapper = shallow(<ExpenseList expenses={expenses} />);
     expect(wrapper).toMatchSnapshot()
